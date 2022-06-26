c2c3579aaf7fe343a0c6e548bb4c948513485db1
Diogo Costa <2201720@my.ipleiria.pt>
2022-06-26 22:57:17 +0100

    Commit com as janela atualizadas (cliente, registoCliente, Transacao, registoTransacao)

--- src/main/java/vista/JanelaTransacoes.java
+++ src/main/java/vista/JanelaTransacoes.java
@@ -1,21 +1,65 @@
 package vista;
 
+import modelo.*;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.util.List;
 
 public class JanelaTransacoes extends JFrame {
     private JButton novaTransaçãoButton;
-    private JList listaTransaçoes;
     private JPanel painelJanelaTransacoes;
+    private JTable table1;
+
+    DefaultTableModel dm;
 
     public JanelaTransacoes(String title) {
         super(title);
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setContentPane(painelJanelaTransacoes);
         pack();
+
+        createColumns();
+        populateTabel();
+        novaTransaçãoButton.addActionListener(this::btnRegistarNovaTransacao);
     }
 
-    public static void main(String[] args) {
-        new JanelaTransacoes("Janela Transações").setVisible(true);
+    private void btnRegistarNovaTransacao(ActionEvent evt){
+        new JanelaRegistoTransacao("Janela Registo Trasação", this).setVisible(true);
+    }
+
+    private void createColumns() {
+        dm = (DefaultTableModel) table1.getModel();
+        dm.addColumn("Nome Cliente");
+        dm.addColumn("NIF");
+        dm.addColumn("Valor");
+        dm.addColumn("Tipo");
+    }
+
+    private void populateTabel() {
+        List<Transacao> listaTrasacoes = GestorTrasacoes.INSTANCE.getTransacoes();
+
+
+        for(Transacao transacao : listaTrasacoes){
+            if(transacao.isCompra()){
+                String[] rowData = {
+                        String.valueOf(transacao.getCliente().getNome()),
+                        String.valueOf(transacao.getCliente().getNif()),
+                        String.valueOf(transacao.getValor()),
+                        "Compra"
+                };
+                dm.addRow(rowData);
+            } else{
+                String[] rowData = {
+                        String.valueOf(transacao.getCliente().getNome()),
+                        String.valueOf(transacao.getCliente().getNif()),
+                        String.valueOf(transacao.getValor()),
+                        "Venda"
+                };
+                dm.addRow(rowData);
+            }
+        }
     }
 }
